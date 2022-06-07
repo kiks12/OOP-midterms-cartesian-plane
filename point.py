cf8dcdb62fe6109fdf618b7468388d6b51c09b7a
Francis James Tolentino <francistolentino1107@gmail.com>
2022-06-07 11:52:35 +0800

    setter for x and y

--- point.py
+++ point.py
@@ -1,4 +1,5 @@
 
+
 class Point:
 
     def __init__(self, x: int, y: int, name: str, index: int):
@@ -9,6 +10,12 @@ class Point:
 
     def getCoordinates(self):
         return [self.__x, self.__y]
+
+    def setX(self, x):
+        self.__x = x
+
+    def setY(self, y):
+        self.__y = y
 
     def __str__(self):
         return f"Point {self.name} (x: {self.__x}, y: {self.__y} index: {self.index})"
